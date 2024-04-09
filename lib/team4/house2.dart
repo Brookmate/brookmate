cc979a790392ff65129ce3401f81951f5616ad0d
Sooa Kim <100650266+SooaKim127@users.noreply.github.com>
2024-04-09 19:49:55 +0900

    add function isLiked

--- lib/team4/house2.dart
+++ lib/team4/house2.dart
@@ -1,120 +1,148 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class House2 extends StatelessWidget {
-  final String houseName, rating;
-  final double numRating;
-  final int numReviews, numBeds, numBath, rent, numKm;
-  final bool isFreeElec;
 
-  const House2(
-      {super.key,
-      required this.houseName,
-      required this.numRating,
-      required this.rating,
-      required this.numReviews,
-      required this.numKm,
-      required this.numBeds,
-      required this.numBath,
-      required this.rent,
-      required this.isFreeElec});
+class House2 extends StatefulWidget {
+ final String houseName, rating;
+ final double numRating;
+ final int numReviews, numBeds, numBath, rent, numKm;
+ final bool isFreeElec;
 
-  final _greyColor = const Color.fromRGBO(206, 206, 206, 100);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Row(children: [
-        Flexible(
-          flex: 3,
-          child: Container(
-            height: 200,
-            //color: Colors.grey,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16.0),
-              color: Colors.grey[350],
-            ),
-          ),
-        ),
-        const SizedBox(
-          width: 10,
-        ),
-        Flexible(
-          flex: 7,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                  //하트 기준으로 좌우 나누기
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      // strqevrry, excellent 기준 상하로 나누기
-                      children: [
-                        Text(houseName,
-                            style: const TextStyle(fontSize: 20, height: 1.1)),
-                        Row(children: [
-                          Container(
-                            decoration: BoxDecoration(
-                                color: _greyColor,
-                                borderRadius: BorderRadius.circular(3)),
-                            padding: const EdgeInsets.all(2),
-                            child: Text('$numRating',
-                                style: const TextStyle(
-                                  fontSize: 10,
-                                )),
-                          ),
-                          const SizedBox(
-                            width: 3,
-                          ),
-                          Text(rating,
-                              style:
-                                  const TextStyle(fontSize: 10, height: 0.8)),
-                          Text(' - $numReviews reviews',
-                              style: const TextStyle(fontSize: 10, height: 0.8))
-                        ])
-                      ],
-                    ),
-                    const Icon(
-                      Icons.favorite_border,
-                      color: Colors.black,
-                      size: 45,
-                    ),
-                  ]),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                const Row(children: [
-                  Icon(
-                    Icons.location_on,
-                    color: Colors.black,
-                    size: 15,
-                  ),
-                  Text(
-                    'New york',
-                    style: TextStyle(fontSize: 13),
-                  ),
-                ]),
-                Text(
-                  '$numKm km from School',
-                  style: const TextStyle(fontSize: 13),
-                )
-              ]),
-              isFreeElec
-                  ? Text(
-                      '$numBeds beds, $numBath bathrooms, free electricity',
-                      style: const TextStyle(fontSize: 13, height: 1.4),
-                      softWrap: true,
-                    )
-                  : Text(
-                      '$numBeds beds, $numBath bathrooms',
-                      softWrap: true,
-                    ),
-              Text('1 month: KRW $rent')
-            ],
-          ),
-        ),
-      ]),
-    );
-  }
+ const House2(
+     {super.key,
+     required this.houseName,
+     required this.numRating,
+     required this.rating,
+     required this.numReviews,
+     required this.numKm,
+     required this.numBeds,
+     required this.numBath,
+     required this.rent,
+     required this.isFreeElec});
+
+
+ @override
+ State<House2> createState() => _House2State();
 }
+
+
+class _House2State extends State<House2> {
+ final _greyColor = const Color.fromRGBO(206, 206, 206, 100);
+ bool isLiked = false;
+
+
+ void toggleLike() {
+   setState(() {
+     isLiked = !isLiked;
+   });
+ }
+
+
+ @override
+ Widget build(BuildContext context) {
+   return Container(
+     padding: const EdgeInsets.symmetric(horizontal: 20),
+     child: Row(children: [
+       Flexible(
+         flex: 3,
+         child: Container(
+           height: 200,
+           decoration: BoxDecoration(
+             borderRadius: BorderRadius.circular(16.0),
+             color: Colors.grey[350],
+           ),
+         ),
+       ),
+       const SizedBox(
+         width: 10,
+       ),
+       Flexible(
+         flex: 7,
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: [
+             Row(
+                 //하트 기준으로 좌우 나누기
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 children: [
+                   Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     // strqevrry, excellent 기준 상하로 나누기
+                     children: [
+                       Text(widget.houseName,
+                           style: const TextStyle(fontSize: 20, height: 1.1)),
+                       Row(children: [
+                         Container(
+                           decoration: BoxDecoration(
+                               color: _greyColor,
+                               borderRadius: BorderRadius.circular(3)),
+                           padding: const EdgeInsets.all(2),
+                           child: Text('${widget.numRating}',
+                               style: const TextStyle(
+                                 fontSize: 10,
+                               )),
+                         ),
+                         const SizedBox(
+                           width: 3,
+                         ),
+                         Text(widget.rating,
+                             style:
+                                 const TextStyle(fontSize: 10, height: 0.8)),
+                         Text(' - ${widget.numReviews} reviews',
+                             style: const TextStyle(fontSize: 10, height: 0.8))
+                       ])
+                     ],
+                   ),
+                   isLiked ?
+                   IconButton(
+                     onPressed: toggleLike,
+                     icon: const Icon(Icons.favorite_border),
+                     color: Colors.black,
+                     iconSize: 40
+                   )
+                   :IconButton(
+                     onPressed: toggleLike,
+                     icon: const Icon(Icons.favorite),
+                     color: Colors.red,
+                     iconSize: 40
+                   ),
+                 ]),
+             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+               const Row(children: [
+                 Icon(
+                   Icons.location_on,
+                   color: Colors.black,
+                   size: 15,
+                 ),
+                 Text(
+                   'New york',
+                   style: TextStyle(fontSize: 13),
+                 ),
+               ]),
+               Text(
+                 '${widget.numKm} km from School',
+                 style: const TextStyle(fontSize: 13),
+               )
+             ]),
+             widget.isFreeElec
+                 ? Text(
+                     '${widget.numBeds} beds, ${widget.numBath} bathrooms, free electricity',
+                     style: const TextStyle(fontSize: 13, height: 1.4),
+                     softWrap: true,
+                   )
+                 : Text(
+                     '${widget.numBeds} beds, ${widget.numBath} bathrooms',
+                     softWrap: true,
+                   ),
+             Text('1 month: KRW ${widget.rent}')
+           ],
+         ),
+       ),
+     ]),
+   );
+ }
+}
+
+
+
