a7d87783efec3b904dc19187bf4fa1dad9157676
lighteko <huijunyi0212@naver.com>
2024-04-21 13:43:25 +0900

    database service modification

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -5,52 +5,59 @@ import 'package:brookmate/services/models/tenant_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  static late final FirebaseFirestore _db;
+  static bool _init = false;
+  static Future init() async {
+    if (_init) return;
+    _db = FirebaseFirestore.instance;
+    _init = true;
+    return _db;
+  }
 
   // Create
-  void addHouse(House houseData) async {
+  static void addHouse(House houseData) async {
     await _db.collection("Houses").add(houseData.toMap());
   }
 
-  void addOwner(Owner ownerData) async {
+  static void addOwner(Owner ownerData) async {
     await _db.collection("Owners").add(ownerData.toMap());
   }
 
-  void addTenant(Tenant tenantData) async {
+  static void addTenant(Tenant tenantData) async {
     await _db.collection("Tenants").add(tenantData.toMap());
   }
 
-  void addPersona(Persona personaData) async {
+  static void addPersona(Persona personaData) async {
     await _db.collection("Employees").add(personaData.toMap());
   }
 
   // Read (Single)
-  Future<House> getHouse(DocumentReference ref) async {
+  static Future<House> getHouse(DocumentReference ref) async {
     DocumentSnapshot<Map<String, dynamic>> snapshot =
         await _db.collection("Houses").doc(ref.id).get();
     return House.fromDocumentSnapshot(snapshot);
   }
 
-  Future<Owner> getOwner(DocumentReference ref) async {
+  static Future<Owner> getOwner(DocumentReference ref) async {
     DocumentSnapshot<Map<String, dynamic>> snapshot =
         await _db.collection("Owners").doc(ref.id).get();
     return Owner.fromDocumentSnapshot(snapshot);
   }
 
-  Future<Tenant> getTenant(DocumentReference ref) async {
+  static Future<Tenant> getTenant(DocumentReference ref) async {
     DocumentSnapshot<Map<String, dynamic>> snapshot =
         await _db.collection("Tenants").doc(ref.id).get();
     return Tenant.fromDocumentSnapshot(snapshot);
   }
 
-  Future<Persona> getPersona(DocumentReference ref) async {
+  static Future<Persona> getPersona(DocumentReference ref) async {
     DocumentSnapshot<Map<String, dynamic>> snapshot =
         await _db.collection("Persona").doc(ref.id).get();
     return Persona.fromDocumentSnapshot(snapshot);
   }
 
   // Read (Multiple)
-  Future<List<House>> getHousesSnapshot() async {
+  static Future<List<House>> getHousesSnapshot() async {
     QuerySnapshot<Map<String, dynamic>> snapshot =
         await _db.collection("Houses").get();
     return snapshot.docs
@@ -58,7 +65,7 @@ class DatabaseService {
         .toList();
   }
 
-  Future<List<Owner>> getOwnersSnapshot() async {
+  static Future<List<Owner>> getOwnersSnapshot() async {
     QuerySnapshot<Map<String, dynamic>> snapshot =
         await _db.collection("Owners").get();
     return snapshot.docs
@@ -66,7 +73,7 @@ class DatabaseService {
         .toList();
   }
 
-  Future<List<Tenant>> getTenantsSnapshot() async {
+  static Future<List<Tenant>> getTenantsSnapshot() async {
     QuerySnapshot<Map<String, dynamic>> snapshot =
         await _db.collection("Tenants").get();
     return snapshot.docs
@@ -74,7 +81,7 @@ class DatabaseService {
         .toList();
   }
 
-  Future<List<Tenant>> getPersonaSnapshot() async {
+  static Future<List<Tenant>> getPersonaSnapshot() async {
     QuerySnapshot<Map<String, dynamic>> snapshot =
         await _db.collection("Persona").get();
     return snapshot.docs
@@ -83,22 +90,22 @@ class DatabaseService {
   }
 
   // Update
-  void updateHouse(House houseData) async {
+  static void updateHouse(House houseData) async {
     await _db.collection("Houses").doc(houseData.id).update(houseData.toMap());
   }
 
-  void updateOwner(Owner ownerData) async {
+  static void updateOwner(Owner ownerData) async {
     await _db.collection("Owners").doc(ownerData.id).update(ownerData.toMap());
   }
 
-  void updateTenant(Tenant tenantData) async {
+  static void updateTenant(Tenant tenantData) async {
     await _db
         .collection("Tenants")
         .doc(tenantData.id)
         .update(tenantData.toMap());
   }
 
-  void updatePersona(Persona personaData) async {
+  static void updatePersona(Persona personaData) async {
     await _db
         .collection("Employees")
         .doc(personaData.id)
@@ -106,19 +113,19 @@ class DatabaseService {
   }
 
   // Delete
-  void deleteHouse(String houseId) async {
+  static void deleteHouse(String houseId) async {
     await _db.collection("Houses").doc(houseId).delete();
   }
 
-  void deleteOwner(String ownerId) async {
+  static void deleteOwner(String ownerId) async {
     await _db.collection("Owners").doc(ownerId).delete();
   }
 
-  void deleteTenant(String tenantId) async {
+  static void deleteTenant(String tenantId) async {
     await _db.collection("Tenants").doc(tenantId).delete();
   }
 
-  void deletePersona(String personaId) async {
+  static void deletePersona(String personaId) async {
     await _db.collection("Employees").doc(personaId).delete();
   }
 }
---
+++ test/db_test.dart
@@ -0,0 +1,32 @@
+import "package:brookmate/firebase_options.dart";
+import "package:brookmate/services/database_service.dart";
+import "package:firebase_core/firebase_core.dart";
+import "package:flutter/material.dart";
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await DatabaseService.init();
+  runApp(const DBTest());
+}
+
+class DBTest extends StatefulWidget {
+  const DBTest({super.key});
+
+  @override
+  State<DBTest> createState() => _DBTestState();
+}
+
+class _DBTestState extends State<DBTest> {
+  @override
+  Widget build(BuildContext context) {
+    late final houses = DatabaseService.getHousesSnapshot();
+    houses.then((value) => {for (int i = 0; i < 1; i++) print(value[i])});
+    return const MaterialApp(
+        home: Scaffold(
+      body: Column(),
+    ));
+  }
+}
