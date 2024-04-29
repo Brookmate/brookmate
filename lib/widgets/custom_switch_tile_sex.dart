41c77cab48649f0da13bdf61e32854bb9544b314
= <=>
2024-04-29 23:18:36 +0900

    <toggle switch>

--- lib/widgets/custom_switch_tile_sex.dart
+++ lib/widgets/custom_switch_tile_sex.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class CustomSwitchTileSex extends StatelessWidget {
   final String title;
   final bool value;
   final Function(bool) onChanged;
+  final bool isSexSwitch;
 
   const CustomSwitchTileSex({
     required this.title,
     required this.value,
     required this.onChanged,
+    this.isSexSwitch = false,
     super.key,
   });
 
@@ -20,11 +23,30 @@ class CustomSwitchTileSex extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Text(value ? 'Female' : 'Male'),
-          Switch.adaptive(
-            value: value,
-            activeColor: Colors.red,
-            onChanged: onChanged,
-          ),
+          SizedBox(width: 10), // Add some space between text and switch
+          isSexSwitch
+              ? ToggleSwitch(
+                  minWidth: 90.0,
+                  initialLabelIndex: value ? 1 : 0,
+                  cornerRadius: 20.0,
+                  activeFgColor: Colors.white,
+                  inactiveBgColor: Colors.grey,
+                  inactiveFgColor: Colors.white,
+                  totalSwitches: 2,
+                  labels: ['Male', 'Female'],
+                  activeBgColors: const [
+                    [Color(0xff9C0000)],
+                    [Color(0xff9C0000)]
+                  ],
+                  onToggle: (index) {
+                    onChanged(index == 1 ? true : false);
+                  },
+                )
+              : Switch.adaptive(
+                  value: value,
+                  activeColor: const Color(0xff9C0000),
+                  onChanged: onChanged,
+                ),
         ],
       ),
       onTap: () => onChanged(!value),
