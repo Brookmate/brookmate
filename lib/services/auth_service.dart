6efcf43ab746b7d6016084215321314fabe60b00
lighteko <huijuyi0212@naver.com>
2024-04-30 11:20:32 +0900

    auth service initialize

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,4 +1,9 @@
+import "package:brookmate/services/database_service.dart";
+import "package:brookmate/services/models/owner_model.dart";
+import "package:brookmate/services/models/tenant_model.dart";
 import "package:firebase_auth/firebase_auth.dart";
+
+enum Users { tenant, owner }
 
 class AuthService {
   static late final FirebaseAuth _auth;
@@ -10,9 +15,50 @@ class AuthService {
     return _auth;
   }
 
-  // TODO: Build Sign Up method
+  static User getCurrentUser() {
+    User? user = _auth.currentUser;
+    if (user == null) throw Exception("User not logged in");
+    return user;
+  }
 
-  // TODO: Build Log In method
+  static void signUpByEmail(
+      Users user, String name, String email, String password) async {
+    await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    switch (user) {
+      case Users.owner:
+        Owner owner = Owner(email: email, name: name, houses: []);
+        DatabaseService.addOwner(owner);
+        break;
+      case Users.tenant:
+        Tenant tenant = Tenant(
+            email: email,
+            name: name,
+            likedHouses: [],
+            likedTenants: [],
+            myReviews: [],
+            allowSearch: true,
+            brookmates: []);
+        DatabaseService.addTenant(tenant);
+        break;
+    }
+  }
 
-  // TODO: Build
+  static void logInByEmail(String email, String password) async {
+    await _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  // TODO: Build Change Password method
+
+  // TODO: Build change name method
+
+  // TODO: Build change email method
+
+  // TODO: Build log out method
+
+  // TODO: Build remove user method
+
+  // TODO: Build  Add Social Account method
+
+  // TODO:
 }
