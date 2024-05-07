8276d3662ef96215af7ea2368d35fdd0b16899a4
hyeonhhwang <hyeonha.hwang@stonybrook.edu>
2024-05-07 18:07:03 +0900

    switch tile ToggleSwtich

--- lib/widgets/custom_switch_tile.dart
+++ lib/widgets/custom_switch_tile.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class CustomSwitchTile extends StatelessWidget {
   final String title;
   final bool value;
   final Function(bool) onChanged;
+  final bool toggleSwitch;
 
   const CustomSwitchTile({
     required this.title,
     required this.value,
     required this.onChanged,
+    this.toggleSwitch = false, // default value for toggleSwitch
     super.key,
   });
 
@@ -20,11 +23,30 @@ class CustomSwitchTile extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Text(value ? 'Yes' : 'No'),
-          Switch.adaptive(
-            value: value,
-            activeColor: Color(0xff9C0000),
-            onChanged: onChanged,
-          ),
+          SizedBox(width: 10), // Add some space between text and switch
+          toggleSwitch
+              ? ToggleSwitch(
+                  minWidth: 90.0,
+                  cornerRadius: 20.0,
+                  activeBgColors: [
+                    [Color(0xff9C0000)!]
+                  ],
+                  activeFgColor: Colors.white,
+                  inactiveBgColor: Colors.grey,
+                  inactiveFgColor: Colors.white,
+                  initialLabelIndex: value ? 0 : 1,
+                  totalSwitches: 2,
+                  labels: ['Yes', 'No'],
+                  radiusStyle: true,
+                  onToggle: (index) {
+                    onChanged(index == 0 ? true : false);
+                  },
+                )
+              : Switch.adaptive(
+                  value: value,
+                  activeColor: const Color(0xff9C0000),
+                  onChanged: onChanged,
+                ),
         ],
       ),
       onTap: () => onChanged(!value),
